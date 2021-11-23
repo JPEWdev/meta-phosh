c070c187cbf5b748ce8eb21c80ea3bd1bb8cd91e
Joshua Watt <JPEWhacker@gmail.com>
2021-11-23 15:53:34 -0600

    ci: Add additional debugging for qemu
    Adds additional debugging output to try and figure out why the
    squeekboard test is failing
    
    Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>

--- ci/qemux86-64/test_boot_qemux86-64.py
+++ ci/qemux86-64/test_boot_qemux86-64.py
@@ -3,12 +3,18 @@ def test_boot(shell_command):
 
 
 def test_phoc(shell_command):
+    output, _, _ = shell_command.run("ps")
+    print("\n".join(output))
     shell_command.run_check("pidof phoc")
 
 
 def test_phosh(shell_command):
+    output, _, _ = shell_command.run("ps")
+    print("\n".join(output))
     shell_command.run_check("pidof phosh")
 
 
 def test_squeekboard(shell_command):
+    output, _, _ = shell_command.run("ps")
+    print("\n".join(output))
     shell_command.run_check("pidof squeekboard")
